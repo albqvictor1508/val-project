8f8a93f8e255c9ca563ae5c0a09b9d154d1b9d64
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 10:55:00 -0300

    wip: address repo created

---
+++ src/main/java/com/val/project/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.val.project.repository;
+
+import com.val.project.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {}
