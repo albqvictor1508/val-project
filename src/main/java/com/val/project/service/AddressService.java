fdcd214e65906276d121137e1151554f6b0a522d
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 11:04:25 -0300

    wip: address service created

---
+++ src/main/java/com/val/project/service/AddressService.java
@@ -0,0 +1,9 @@
+package com.val.project.service;
+
+import com.val.project.repository.AddressRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+    private AddressRepository addressRepository;
+}
