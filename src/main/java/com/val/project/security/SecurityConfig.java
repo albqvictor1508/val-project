109dfd0e3b2c5e2956737269f3903b97b3721a34
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 08:33:10 -0300

    wip: add auth configuration bean

--- src/main/java/com/val/project/security/SecurityConfig.java
+++ src/main/java/com/val/project/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.val.project.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,5 +24,10 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .cors(cors -> cors.disable()).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
