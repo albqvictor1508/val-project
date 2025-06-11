c8283c5ff05f9d1035a2acb04687302ffc9075e2
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 11:03:33 -0300

    wip: user service created

---
+++ src/main/java/com/val/project/service/UserService.java
@@ -0,0 +1,9 @@
+package com.val.project.service;
+
+import com.val.project.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+}
