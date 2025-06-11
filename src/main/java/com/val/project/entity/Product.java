8969b3180219c1c0d015409519b71c0d6f7c125b
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 10:52:17 -0300

    wip: add lombok notations

--- src/main/java/com/val/project/entity/Product.java
+++ src/main/java/com/val/project/entity/Product.java
@@ -1,9 +1,17 @@
 package com.val.project.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/val/project/entity/ProductCart.java
+++ src/main/java/com/val/project/entity/ProductCart.java
@@ -1,11 +1,19 @@
 package com.val.project.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "product_cart")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
