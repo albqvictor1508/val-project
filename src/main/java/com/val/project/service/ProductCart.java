d6401a99d87396394a76f355d02d5232af73e14e
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 11:11:10 -0300

    wip: product cart service created

---
+++ src/main/java/com/val/project/service/ProductCart.java
@@ -0,0 +1,9 @@
+package com.val.project.service;
+
+import com.val.project.repository.ProductCartRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductCart {
+    private ProductCartRepository productCartRepository;
+}
