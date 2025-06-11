76416bbcc997588d54f9512cc39a1ae26a954fc7
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 11:10:08 -0300

    wip: product service created

---
+++ src/main/java/com/val/project/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.val.project.service;
+
+import com.val.project.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+}
