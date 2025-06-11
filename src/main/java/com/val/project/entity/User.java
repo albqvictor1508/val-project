8f2750975035cd30ebe4f043bdd9ae8fd6c1ce8d
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 08:22:05 -0300

    wip: user, first entity

---
+++ src/main/java/com/val/project/entity/User.java
@@ -0,0 +1,27 @@
+package com.val.project.entity;
+
+import jakarta.persistence.Entity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
