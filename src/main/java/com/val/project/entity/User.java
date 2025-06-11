d07f42204f5ed79b41443da600365af7f0642612
albqvictor1508 <albq.victor@gmail.com>
2025-06-11 08:47:18 -0300

    wip: creating the user entity

--- src/main/java/com/val/project/entity/User.java
+++ src/main/java/com/val/project/entity/User.java
@@ -1,6 +1,8 @@
 package com.val.project.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -9,7 +11,24 @@ import java.util.List;
 
 @Entity
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Email
+    @Column(unique = true, length = 120, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @NotBlank
+    @Column(length = 80, nullable = false)
+    private String city;
+    @NotBlank
+    @Column(length = 80, nullable = false)
+    private String uf;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
