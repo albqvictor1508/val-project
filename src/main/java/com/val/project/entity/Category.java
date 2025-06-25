4b597ba9fb124f173c1b88fd2af59a251c790023
albqvictor1508 <albq.victor@gmail.com>
2025-06-25 09:50:52 -0300

    chore: category entity created

---
+++ src/main/java/com/val/project/entity/Category.java
@@ -0,0 +1,26 @@
+package com.val.project.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(unique = true)
+  private String name;
+  @Column(unique = true)
+  private String slug;
+}
