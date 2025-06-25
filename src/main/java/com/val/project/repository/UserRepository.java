1b034bb65f7a14c0b1affc14e61fa4e2d3de686e
albqvictor1508 <albq.victor@gmail.com>
2025-06-25 08:25:53 -0300

    wip: find by email and find by username on user service

--- src/main/java/com/val/project/repository/UserRepository.java
+++ src/main/java/com/val/project/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.val.project.repository;
 
 import com.val.project.entity.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email);
+
+  Optional<User> findByUsername(String username);
+}
--- src/main/java/com/val/project/service/UserService.java
+++ src/main/java/com/val/project/service/UserService.java
@@ -9,11 +9,28 @@ public class UserService {
   private UserRepository userRepository;
 
   private User save(User u) {
+    userRepository.findByEmail(u.getEmail());
+    userRepository.findByUsername(u.getEmail());
     return userRepository.save(u);
   }
 
   private User findById(Long userId) {
     return userRepository.findById(userId)
         .orElseThrow(() -> new RuntimeException("user with id %s not exists".formatted(userId)));
+  }
+
+  private User findByEmail(String email) {
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new RuntimeException("user with id %s not exists".formatted(email)));
+  }
+
+  private User findByUsername(String username) {
+    return userRepository.findByUsername(username)
+        .orElseThrow(() -> new RuntimeException("user with id %s not exists".formatted(username)));
+  }
+
+  private void delete(Long userId) {
+    User u = findById(userId);
+    userRepository.delete(u);
   }
 }
