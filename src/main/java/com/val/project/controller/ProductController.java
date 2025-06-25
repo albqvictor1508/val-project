897525210358418e1c6c13f26e21ab7d801c1dde
albqvictor1508 <albq.victor@gmail.com>
2025-06-25 13:49:35 -0300

    chore: product controller find by category slug

--- src/main/java/com/val/project/controller/ProductController.java
+++ src/main/java/com/val/project/controller/ProductController.java
@@ -1,13 +1,24 @@
 package com.val.project.controller;
 
+import com.val.project.entity.Product;
 import com.val.project.service.ProductService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    @Autowired
-    private ProductService productService;
+  @Autowired
+  private ProductService productService;
+
+  @GetMapping("/:categorySlug/products")
+  public List<Product> findByCategorySlug(@Param("categorySlug") String categorySlug) {
+    return productService.findByCategorySlug(categorySlug);
+  }
 }
