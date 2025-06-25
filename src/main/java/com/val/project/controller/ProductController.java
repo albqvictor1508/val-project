6e972ac69f8756f53bf6d7e80e4beceeba1177a4
albqvictor1508 <albq.victor@gmail.com>
2025-06-25 13:52:52 -0300

    chore: save and delete route created

--- src/main/java/com/val/project/controller/ProductController.java
+++ src/main/java/com/val/project/controller/ProductController.java
@@ -3,11 +3,16 @@ package com.val.project.controller;
 import com.val.project.entity.Product;
 import com.val.project.service.ProductService;
 
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +25,15 @@ public class ProductController {
   @GetMapping("/:categorySlug/products")
   public List<Product> findByCategorySlug(@Param("categorySlug") String categorySlug) {
     return productService.findByCategorySlug(categorySlug);
+  }
+
+  @PostMapping
+  public Product save(@Valid @RequestBody Product p) {
+    return productService.save(p);
+  }
+
+  @DeleteMapping
+  public void delete(Long productId) {
+    productService.delete(productId);
   }
 }
