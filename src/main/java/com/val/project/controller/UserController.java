b21cc386767eeabdbd5faff44a91764e27db2378
albqvictor1508 <albq.victor@gmail.com>
2025-06-26 12:38:26 -0300

    chore: add validation on create user route

--- src/main/java/com/val/project/controller/UserController.java
+++ src/main/java/com/val/project/controller/UserController.java
@@ -2,10 +2,14 @@ package com.val.project.controller;
 
 import com.val.project.entity.User;
 import com.val.project.service.UserService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +20,7 @@ public class UserController {
   private UserService userService;
 
   @PostMapping
-  public User save(User u) {
+  public User save(@Valid @RequestBody User u) {
     return userService.save(u);
   }
 
