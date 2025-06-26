e061e1e10cd59d3dfecd36d5aa20d993d981d16a
albqvictor1508 <albq.victor@gmail.com>
2025-06-26 09:09:04 -0300

    wip: renaming product cart to cart

--- src/main/java/com/val/project/entity/Cart.java
+++ src/main/java/com/val/project/entity/Cart.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductCart {
+public class Cart {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
--- src/main/java/com/val/project/repository/ProductCartRepository.java
+++ src/main/java/com/val/project/repository/ProductCartRepository.java
@@ -1,8 +1,9 @@
 package com.val.project.repository;
 
-import com.val.project.entity.ProductCart;
+import com.val.project.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductCartRepository extends JpaRepository<ProductCart, Long> {}
+public interface ProductCartRepository extends JpaRepository<Cart, Long> {
+}
