2a6f2160cf4c2198985c922ae1f1550ab41e1254
albqvictor1508 <albq.victor@gmail.com>
2025-06-27 20:27:07 -0300

    chore: save category route created

--- src/main/java/com/val/project/controller/CategoryController.java
+++ src/main/java/com/val/project/controller/CategoryController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.val.project.entity.Category;
 import com.val.project.repository.CategoryRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/category")
@@ -20,4 +24,10 @@ public class CategoryController {
   public List<Category> findAll() {
     return categoryRepository.findAll();
   }
+
+  @PostMapping
+  public Category save(@Valid @RequestBody Category body) {
+    return categoryRepository.save(body);
+  }
+
 }
