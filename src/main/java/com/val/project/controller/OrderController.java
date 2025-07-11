3a68d91c20dde5c420e253e7f8c3873794356b64
albqvictor1508 <albq.victor@gmail.com>
2025-07-11 17:10:00 -0300

    chore: many changes on order

--- src/main/java/com/val/project/controller/OrderController.java
+++ src/main/java/com/val/project/controller/OrderController.java
@@ -1,10 +1,21 @@
 package com.val.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.val.project.entity.Order;
+import com.val.project.service.OrderService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class OrderController {
   @Autowired
   private OrderService orderService;
+
+  public ResponseEntity<Order> createOrder(@Valid @RequestBody Order o) {
+    return ResponseEntity.ok(o);
+  }
 }
