a230628b0828957c1b046f6b3516fa4685554d5a
albqvictor1508 <albq.victor@gmail.com>
2025-07-11 15:27:27 -0300

    chore: create order item entity

---
+++ src/main/java/com/val/project/entity/OrderItem.java
@@ -0,0 +1,43 @@
+package com.val.project.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "order_id", nullable = false)
+  private Order order;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product;
+
+  @Column(nullable = false)
+  private Integer quantity;
+
+  @Column(nullable = false)
+  private Double price;
+
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @PrePersist
+  protected void onCreate() {
+    this.createdAt = LocalDateTime.now();
+  }
+}
