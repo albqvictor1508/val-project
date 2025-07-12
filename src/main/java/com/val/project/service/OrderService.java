4ff687bcf5d4b5d4dc325aa436021a822e15e9e7
albqvictor1508 <albq.victor@gmail.com>
2025-07-12 10:01:16 -0300

    fix: add service notation on order service

--- src/main/java/com/val/project/service/OrderService.java
+++ src/main/java/com/val/project/service/OrderService.java
@@ -1,10 +1,12 @@
 package com.val.project.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.val.project.entity.Order;
 import com.val.project.repository.OrderRepository;
 
+@Service
 public class OrderService {
   @Autowired
   private OrderRepository orderRepository;
