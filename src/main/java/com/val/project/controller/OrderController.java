d414747a7797f7a5d4ecfe7163ffaf5865bbbcd4
albqvictor1508 <albq.victor@gmail.com>
2025-07-12 09:59:29 -0300

    wip: add route notation

--- src/main/java/com/val/project/controller/OrderController.java
+++ src/main/java/com/val/project/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.val.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +21,14 @@ public class OrderController {
   private OrderService orderService;
 
   // TODO: CRIAR O ORDER QUANDO O CHECKOUT FOR FEITO, E CRIAR UM DTO PRA ISSO
+  @PostMapping
   public ResponseEntity<Order> createOrder(@Valid @RequestBody Order o) {
     orderService.createOrder(o);
     return ResponseEntity.ok(o);
   }
 
-  public ResponseEntity<?> deleteOrder(@Valid @RequestBody Long orderId) {
+  @DeleteMapping("/{orderId}")
+  public ResponseEntity<?> deleteOrder(@PathVariable Long orderId) {
     orderService.delete(orderId);
     return ResponseEntity.status(200).build();
   }
