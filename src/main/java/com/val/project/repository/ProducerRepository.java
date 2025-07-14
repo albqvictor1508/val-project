97743e957fafa431231ca27221243683697c03d3
albqvictor1508 <albq.victor@gmail.com>
2025-07-14 18:06:21 -0300

    chore: add producer repo

---
+++ src/main/java/com/val/project/repository/ProducerRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.val.project.entity.Producer;
+
+public interface ProducerRepository extends JpaRepository<Producer, Long> {
+}
